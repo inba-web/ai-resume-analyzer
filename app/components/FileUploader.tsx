33119189adc651fec4e680bedc48c2657f76cab2
Inba <inba.web@gmail.com>
2026-01-17 19:25:35 +0530

    upload file and text component implemented

---
+++ app/components/FileUploader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FileUploader = () => {
+  return (
+    <div className='w-full gradient-border'>
+        FileUploader
+    </div>
+  )
+}
+
+export default FileUploader
